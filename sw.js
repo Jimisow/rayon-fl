4a5b436e9307e58e824f6ad025f8598017f41436
Jimisow <jimi.carreno@gmail.com>
2026-05-29 23:51:13 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'rayon-frais-v1';
-const ASSETS = ['./index.html', './produits.json', './manifest.json'];
+const CACHE_NAME = 'rayon-frais-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './produits.json',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
